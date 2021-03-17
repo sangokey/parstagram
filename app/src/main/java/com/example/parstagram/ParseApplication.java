b4101d601e2a9df2c269b16ab31b9fb680b86ef4
Sang Ok Suh <sangoksuh@hotmail.com>
2021-03-17 00:41:59 -0700

    Defined Data Object and Query

--- app/src/main/java/com/example/parstagram/ParseApplication.java
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -3,12 +3,16 @@ package com.example.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("RlMlE58blpULbAkb6qtXEk7n2HODYzsTbVrxXxwq")
